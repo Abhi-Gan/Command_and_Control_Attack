722705caa77d698c00c7046d1491c68f2d2b556c
abhi <abhinav.ganesh@utexas.edu>
2024-11-01 11:17:56 -0500

    make sure key is hexadecimal

--- symm_keygen.py
+++ symm_keygen.py
@@ -3,8 +3,8 @@ import os
 def generate_key():
     symmetric_key = os.urandom(32)
     with open("symm_key.txt", "wb") as f:
-        # hex_key = bytes(symmetric_key).hex()
-        f.write(symmetric_key)
+        hex_key = bytes(symmetric_key).hex()
+        f.write(hex_key)
 
 if __name__ == '__main__':
     generate_key()
