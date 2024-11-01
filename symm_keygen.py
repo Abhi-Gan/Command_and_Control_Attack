bae2d5eb6ac5e59df5ce4fcb0c08b6585489d84f
abhi <abhinav.ganesh@utexas.edu>
2024-11-01 11:19:45 -0500

    fix: symm keygen hexadecimal

--- symm_keygen.py
+++ symm_keygen.py
@@ -4,7 +4,7 @@ def generate_key():
     symmetric_key = os.urandom(32)
     with open("symm_key.txt", "wb") as f:
         hex_key = bytes(symmetric_key).hex()
-        f.write(hex_key)
+        f.write(hex_key.encode()) # symmetric_key
 
 if __name__ == '__main__':
     generate_key()
